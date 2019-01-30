dbddd27de8935da1f7d92caaeebcec8bab33cd24
zongzhenh <huangzongzhuan@gmail.com>
2019-01-30 11:15:46 +0800

    1

--- viola/http/response.py
+++ viola/http/response.py
@@ -11,7 +11,7 @@ class HttpResponse(object):
         # 封装 data => HTTP 响应体格式
         # 写到 stream 的 write_buffer
         # 将 stream.c_socket 的监听事件修改成 EventLoop.WRITE
-        # Addition: 超过 100M 的不给发
+        # Addition: 超过 SNDBUF 的不给发
         self.wrapper_response(resp_data)
         self.stream.write_buffer.append(resp_data)
         events = EventLoop.WRITE
