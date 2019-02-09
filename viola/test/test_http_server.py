ee3119892b991cd97bc158948fe3e0266e57e715
zongzhenh <huangzongzhuan@gmail.com>
2019-02-09 18:40:43 +0800

    1

--- viola/test/test_http_server.py
+++ viola/test/test_http_server.py
@@ -2,6 +2,7 @@ from viola.event_loop import EventLoop
 from viola.http.server import HTTPServer
 from viola.scheduler import Scheduler
 from viola.test.test_http_app import app
+import os
 
 
 if __name__ == '__main__':
@@ -11,6 +12,6 @@ if __name__ == '__main__':
     server.set_handler(app)
     server.bind(host="10.211.55.25", port=2333)
     server.listen(9128)
-    # server.start(os.cpu_count())
-    server.start(1)
+    server.start(os.cpu_count())
+    # server.start(1)
     event_loop.start()
