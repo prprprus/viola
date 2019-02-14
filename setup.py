3f5fe0dc959e48a49e52525cb39ff4637fe2527c
zongzhenh <huangzongzhuan@gmail.com>
2019-02-14 12:22:03 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="viola",
-    version="0.3.6",
+    version="0.3.7",
     author="prprprus",
     author_email="huangzongzhuan@gmail.com",
     description="viola is a lightweight and efficient WSGI server.",
